fa5afb21bf14ae0ed55101a11d202c06acecd186
Rick Feng <rickfeng99@gmail.com>
2021-05-18 12:18:53 -0700

    main is set up to intake user input

--- Assignment 1/src/sortingArray.java
+++ Assignment 1/src/sortingArray.java
@@ -1,3 +1,27 @@
+import java.util.*;
+
 public class sortingArray {
 
-}
+ public static void main(String[] args) {
+  int count, temp;
+
+  Scanner input = new Scanner(System.in);
+  System.out.print("type 1 for unit test demo, type 2 for array input ");
+  int mode = input.nextInt();
+  if(mode == 1 ){
+   System.out.println("unit testing");
+  }
+  else{
+   System.out.print("Enter number of elements you want in the array: ");
+   count = input.nextInt();
+   int num[] = new int[count];
+   System.out.println("Enter array elements:");
+   for (int i = 0; i < count; i++)
+   {
+    num[i] = input.nextInt();
+   }
+   input.close();
+  }
+ }
+
+  }
