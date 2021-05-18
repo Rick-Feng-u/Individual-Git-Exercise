e1b9ec9c9d6dbcadedcda9f84e6067735d4314cd
Rick Feng <rickfeng99@gmail.com>
2021-05-18 12:20:50 -0700

    Update sortingArray.java

--- Assignment 1/src/sortingArray.java
+++ Assignment 1/src/sortingArray.java
@@ -8,20 +8,18 @@ public class sortingArray {
   Scanner input = new Scanner(System.in);
   System.out.print("type 1 for unit test demo, type 2 for array input ");
   int mode = input.nextInt();
-  if(mode == 1 ){
+  if (mode == 1) {
    System.out.println("unit testing");
-  }
-  else{
+  } else {
    System.out.print("Enter number of elements you want in the array: ");
    count = input.nextInt();
    int num[] = new int[count];
    System.out.println("Enter array elements:");
-   for (int i = 0; i < count; i++)
-   {
+   for (int i = 0; i < count; i++) {
     num[i] = input.nextInt();
    }
    input.close();
   }
  }
 
-  }
+}
