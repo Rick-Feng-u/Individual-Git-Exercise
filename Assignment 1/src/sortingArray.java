7da20fa003218657395b17991f33a709b6d9f451
Rick Feng <rickfeng99@gmail.com>
2021-05-18 14:15:55 -0700

    update for demo

--- Assignment 1/src/sortingArray.java
+++ Assignment 1/src/sortingArray.java
@@ -21,6 +21,8 @@ public class sortingArray {
    input.close();
    int[] num2 = num.clone();
 
+   // this is a comment for demo
+
    BubbleSort ob = new BubbleSort();
    ob.bubbleSort(num);
    System.out.println("Sorted array(BubbleSort)");
